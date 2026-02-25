65056a144c4d46c5e14b76e42acd9867f70383aa
Rmando42 <aremcaar@outlook.com>
2026-02-25 02:01:42 -0600

    added SortingAlgorithms file

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -3,20 +3,10 @@
 
 public class SortingAlgorithms {
 
-	public static void main(String[] args) {
-		int[] test = {64, 34, 25, 12, 22, 11, 90};
-		SortingAlgorithms.bubble(test);
-
-		for (int num : test) {
-			System.out.print(num + " ");
-		}
-	}
-	
 	// Bubble Sort
 	public static void bubble(int[] arr){
 		int n = arr.length;
 		boolean swapped;
-		int temp;
 	
 		for (int i = 0; i < n - 1; i++){
 			swapped = false;
@@ -25,13 +15,13 @@ public class SortingAlgorithms {
 				if(arr[j] > arr[j + 1]){
 				
 					// Swap arr[j] and arr[j + 1]
-					temp = arr[j];
+					int temp = arr[j];
 					arr[j] = arr[j+ 1];
 					arr[j + 1] = temp;
 					swapped = true;
 				}
 			}
-			if (swapped == false)
+			if (!swapped)
 				break;
 		}
 	}
