0007816fd384faeefa6836973ec9ccc96fa38de7
Rmando42 <aremcaar@outlook.com>
2026-02-25 01:41:44 -0600

    added main method

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -2,6 +2,15 @@
 
 
 public class SortingAlgorithms {
+
+	public static void main(String[] args) {
+		int[] test = {64, 34, 25, 12, 22, 11, 90};
+		SortingAlgorithms.bubble(test);
+
+		for (int num : test) {
+			System.out.print(num + " ");
+		}
+	}
 	
 	// Bubble Sort
 	public static void bubble(int[] arr){
