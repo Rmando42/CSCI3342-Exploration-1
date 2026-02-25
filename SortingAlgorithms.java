718339262c252d0d23be4843fe8556c1bac9aa75
Rmando42 <aremcaar@outlook.com>
2026-02-25 01:29:29 -0600

    corrections

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -4,14 +4,15 @@
 public class SortingAlgorithms {
 	
 	// Bubble Sort
-	public static void bubbleSort(int[] arr){
+	public static void bubble(int[] arr){
 		int n = arr.length;
 		boolean swapped;
+		int temp;
 	
 		for (int i = 0; i < n - 1; i++){
 			swapped = false;
 
-			for(j = 0; j < n - i - 1; j++){
+			for(int j = 0; j < n - i - 1; j++){
 				if(arr[j] > arr[j + 1]){
 				
 					// Swap arr[j] and arr[j + 1]
