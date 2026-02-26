264c52e7b086c6b0d9761bb8dfb1c348f5579d16
Rmando42 <aremcaar@outlook.com>
2026-02-26 13:28:09 -0600

    Added Quicksort

--- SortingComparisons.java
+++ SortingComparisons.java
@@ -2,9 +2,10 @@ public class SortingComparisons{
     public static void main(String[] args) {
 		int[] test = {64, 34, 25, 12, 22, 11, 90};
 		SortingAlgorithms.bubble(test);
+        SortingAlgorithms.quick(test);
 
 		int[] test2 = {64, 34, 25, 12, 22, 11, 90};
-		SortingAlgorithms.insertion(test2)
+		SortingAlgorithms.insertion(test2);
 
 		for (int num : test) {
 			System.out.print(num + " ");
